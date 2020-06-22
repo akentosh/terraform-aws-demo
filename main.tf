de08f24f7aedf2ddb3007757a0e66d5317312460
Michael Tharpe <mtharpe@hashicorp.com>
2020-06-22 17:37:20 -0400

    Update name to less than 8 char per requirements

--- main.tf
+++ main.tf
@@ -246,7 +246,7 @@ resource "aws_db_instance" "oracle" {
   engine                 = "oracle-ee"
   engine_version         = "12.1.0.2.v19"
   instance_class         = "db.m5.large"
-  name                   = "${var.user}DemoDB"
+  name                   = "${var.user}"
   username               = var.aws_instance_username
   password               = var.aws_instance_password
   vpc_security_group_ids = [aws_security_group.default.id]
